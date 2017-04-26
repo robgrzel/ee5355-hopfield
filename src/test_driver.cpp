d06ec5636d7a5782d6994d0f2b19b85e0afacd82
Lucas Kramer <krame505@umn.edu>
2017-04-26 15:12:34 -0500

    Test driver works

--- src/test_driver.cpp
+++ src/test_driver.cpp
@@ -6,68 +6,75 @@
 #include <vector>
 using namespace std;
 
-#define DEFAULT_SIZE 100
-#define DEFAULT_NUM_VECTORS 10
+#define DEFAULT_SIZE 10000
+#define DEFAULT_NUM_VECTORS 100
 #define DEFAULT_KEY_SIZE_PROP 0.1
 
 int main(int argc, const char *argv[]) {
   if (argc < 2 || argc > 5) {
     cerr << "Usage: " << argv[0] << " <recall algorithm> <data size>(=" << DEFAULT_SIZE << ") <# of data vectors>(=" << DEFAULT_NUM_VECTORS << ") <fraction of vector included in key>(=" << DEFAULT_KEY_SIZE_PROP << ")" << endl;
-                                                                                                                                                                          exit(1);
+    exit(1);
   }
 
   Recall *recall = getRecall(string(argv[1]));
+  cout << " Recall algorithm: " << argv[1] << endl;
   size_t size = DEFAULT_SIZE;
   if (argc >= 3)
     size = atoi(argv[2]);
+  cout << "        Data size: " << size << endl;
   size_t num_vectors = DEFAULT_NUM_VECTORS;
   if (argc >= 4)
     num_vectors = atoi(argv[3]);
+  cout << "# of data vectors: " << num_vectors << endl;
   float key_size_prop = DEFAULT_KEY_SIZE_PROP;
   if (argc >= 5)
     key_size_prop = atof(argv[4]);
-  size_t key_size = size * key_size_prop;
+  cout << "         Key size: " << size * key_size_prop << endl;
 
-  cout << "Generating test data... ";
+  cout << "Generating test data... " << flush;
   auto t1 = chrono::high_resolution_clock::now();
   vector<vector<bool>> data(num_vectors, vector<bool>(size));
   vector<vector<bool>> keys(num_vectors, vector<bool>(size, false));
   for (size_t i = 0; i < num_vectors; i++) {
     for (size_t j = 0; j < size; j++) {
       data[i][j] = rand() % 2;
-      if ((i + j) % key_size == 0)
+      if ((i + j) % (size_t)(1 / key_size_prop) == 0)
         keys[i][j] = data[i][j];
     }
   }
   auto t2 = chrono::high_resolution_clock::now();
-  cout << (t2 - t1).count() << " sec" << endl;
+  chrono::duration<double> diff = t2 - t1;
+  cout << diff.count() << " sec" << endl;
 
-  cout << "Training network... ";
+  cout << "Training network... " << flush;
   t1 = chrono::high_resolution_clock::now();
   TrainedHopfieldNetwork net(size, DEFAULT_THRESHOLD, recall, new CPUHebbianTraining());
   for (size_t i = 0; i < num_vectors; i++) {
     net.train(data[i]);
   }
   t2 = chrono::high_resolution_clock::now();
-  cout << (t2 - t1).count() << " sec" << endl;
+  diff = t2 - t1;
+  cout << diff.count() << " sec" << endl;
 
-  cout << "Recalling data... ";
+  cout << "Recalling data... " << flush;
   t1 = chrono::high_resolution_clock::now();
   vector<vector<bool>> results(num_vectors);
   for (size_t i = 0; i < num_vectors; i++) {
     results[i] = net.recall(keys[i]);
   }
   t2 = chrono::high_resolution_clock::now();
-  cout << (t2 - t1).count() << " sec" << endl;
+  diff = t2 - t1;
+  cout << diff.count() << " sec" << endl;
 
-  cout << "Checking accuracy... ";
+  cout << "Checking accuracy... " << flush;
   t1 = chrono::high_resolution_clock::now();
   size_t num_correct = 0;
   for (size_t i = 0; i < num_vectors; i++) {
     num_correct += (results[i] == data[i]);
   }
   t2 = chrono::high_resolution_clock::now();
-  cout << (t2 - t1).count() << " sec" << endl;
+  diff = t2 - t1;
+  cout << diff.count() << " sec" << endl;
   cout << num_correct * 100 / num_vectors << "% correct" << endl;
   
 }
